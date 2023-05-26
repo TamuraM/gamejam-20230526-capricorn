88eeed97264e30c339509aa98935e8ad411225e1
aliaaakuaaa <134474066+aliaaakuaaa@users.noreply.github.com>
2023-05-26 15:33:49 +0900

    ステージ作成　更新

--- Assets/Iijima/Coin.cs
+++ Assets/Iijima/Coin.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    AudioSource audioSource;
+    [SerializeField] AudioClip a;
 
+    private void Start()
+    {
+        audioSource = FindObjectOfType<AudioSource>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        audioSource.PlayOneShot(a);
         Destroy(gameObject);
     }
 
