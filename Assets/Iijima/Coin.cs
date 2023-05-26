f0868cdaeaa83eb074557461573738c1365ea218
aliaaakuaaa <134474066+aliaaakuaaa@users.noreply.github.com>
2023-05-26 16:25:13 +0900

    [add]コインを取ったらコインカウンターが増えるようにした

--- Assets/Iijima/Coin.cs
+++ Assets/Iijima/Coin.cs
@@ -6,13 +6,16 @@ public class Coin : MonoBehaviour
 {
     AudioSource audioSource;
     [SerializeField] AudioClip a;
+    CoinCounter _coinCounter;
 
     private void Start()
     {
         audioSource = FindObjectOfType<AudioSource>();
+        _coinCounter = FindObjectOfType<CoinCounter>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        _coinCounter.CoinCount();
         audioSource.PlayOneShot(a);
         Destroy(gameObject);
     }
