b2929023fcecfc9670e6e34de54f0b044cd0e4fb
TAKAHASHIYUTOGIT <134475821+TAKAHASHIYUTOGIT@users.noreply.github.com>
2023-05-26 16:00:14 +0900

    敵アニメーション追加

--- Assets/Takahashi/Enemy.cs
+++ Assets/Takahashi/Enemy.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     Rigidbody2D _rb;
+    Animator _anim;
+    SpriteRenderer _sr;
     [SerializeField] float _speed;
     float _dir = 1;
     [SerializeField] Transform[] _returnPos;
@@ -13,10 +15,14 @@ public class Enemy : MonoBehaviour
     [SerializeField] float _jumppower;
     [SerializeField] float _cooltime;
     float _isgraundtimer = 0;
+    float _moveAnim;
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+        _sr = GetComponent<SpriteRenderer>();
+        _sr.flipX = true;
     }
 
     // Update is called once per frame
@@ -25,10 +31,12 @@ public class Enemy : MonoBehaviour
         if (transform.position.x > _returnPos[0].position.x)
         {
             _dir = -1;
+            _sr.flipX = false;
         }
         else if (transform.position.x < _returnPos[1].position.x)
         {
             _dir = 1;
+            _sr.flipX = true;
         }
         Debug.Log(_dir);
 
@@ -41,6 +49,7 @@ public class Enemy : MonoBehaviour
         {
             _jump = true;
             _rb.velocity = Vector3.zero;
+            _moveAnim = 0;
             _rb.AddForce(Vector3.up * _jumppower, ForceMode2D.Impulse);
             _isgraundtimer = 0;
         }
@@ -48,10 +57,11 @@ public class Enemy : MonoBehaviour
         {
             _rb.velocity = Vector3.right * _dir * _speed;
             _isgraundtimer += Time.fixedDeltaTime;
+            _moveAnim = 1;
         }
 
         Debug.Log(_isgraundtimer);
 
-        
+        _anim.SetFloat("Move",_moveAnim);
     }
 }
